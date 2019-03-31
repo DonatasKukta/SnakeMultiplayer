d764cae5ea6ecbe63d018e7045d5a05132902e5a
Donatas Kukta <donatas.kukta1@gmail.com>
2019-03-31 21:19:48 +0300

    Created first version of coordinate object

---
+++ SnakeMultiplayer/Services/Coordinate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeMultiplayer.Services
+{
+    public class Coordinate
+    {
+        private int x;
+        private int y;
+        
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Update(CoordDirection direction)
+        {
+            switch (direction)
+            {
+                case CoordDirection.Up:
+                    this.x += 1;
+                    break;
+                case CoordDirection.Right:
+                    this.y += 1;
+                    break;
+                case CoordDirection.Down:
+                    this.x -= 1;
+                    break;
+                case CoordDirection.Left:
+
+                case CoordDirection.None:
+                default:
+                    throw new ArgumentException($"Argument value of enum CoordDirection expected, but {direction} found");
+            }
+        }
+        public (int,int) Get()
+        {
+            return (x, y);
+        }
+    }
+    
+    public enum CoordDirection
+    {
+        None = 0,
+        Up = 1,
+        Right = 2,
+        Down = 3,
+        Left = 4,
+    }
+
+
+}
