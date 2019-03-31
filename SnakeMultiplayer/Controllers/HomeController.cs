7f10fbbc19c135f92df196c04ac237d83476737e
Donatas Kukta <donatas.kukta1@gmail.com>
2019-03-31 16:32:14 +0300

    Initial commit: sample MVC project

---
+++ SnakeMultiplayer/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SnakeMultiplayer.Models;
+using SnakeMultiplayer.Services;
+
+namespace SnakeMultiplayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Messages(string Message, [FromServices]CustomHostedService service)
+        {
+            //await service.AddMessage(Message);
+            service.AddMessage("userxd",Message);
+
+            ViewData["Messages"] = service.getAllMessages();
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SnakeMultiplayer/Services/CustomHostedService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace SnakeMultiplayer.Services
+{
+    public class CustomHostedService : IHostedService
+    {
+        LinkedList<Message> sharedResource = new LinkedList<Message>();
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        
+        public Task AddMessage(string user, string message)
+        {
+            sharedResource.AddLast(new Message(user, message));
+            return Task.CompletedTask;
+        }
+
+        public string getAllMessages()
+        {
+            return getresourcestring();
+        }
+        private string getresourcestring()
+        {
+            string result = "";
+            for (LinkedListNode<Message> i = sharedResource.First; i != null; i = i.Next)
+            {
+                result += i.Value.message + "\n";
+            }
+            return result;
+        }
+
+    }
+
+    class Message
+    {
+        public string user { get; set; }
+        public string message { get; set; }
+
+        public Message(string u, string m)
+        {
+            user = u;
+            message = m;
+        }
+    }
+
+}
