208b1f1edcc6731084f9b40cc3025d382397301c
Donatas Kukta <donatas.kukta1@gmail.com>
2019-04-02 21:52:47 +0300

    Primary snake class

---
+++ SnakeMultiplayer/Services/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeMultiplayer.Services
+{
+    public class Snake
+    {
+        LinkedList<Coordinate> body;
+
+        public Snake(Coordinate initialPosition)
+        {
+            this.body = new LinkedList<Coordinate>();
+            body.AddFirst(initialPosition);
+        }
+    }
+}
