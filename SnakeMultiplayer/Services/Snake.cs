5d2f8a20381a16603e9dab0693cfb086621b0d06
Donatas Kukta <donatas.kukta1@gmail.com>
2019-04-02 22:38:34 +0300

    Initial methods for snake class

--- SnakeMultiplayer/Services/Snake.cs
+++ SnakeMultiplayer/Services/Snake.cs
@@ -7,12 +7,37 @@ namespace SnakeMultiplayer.Services
 {
     public class Snake
     {
-        LinkedList<Coordinate> body;
 
+        public string Name { get; private set; }
+        private LinkedList<Coordinate> body;
+
+        public Snake(string name)
+        {
+            this.Name = name;
+            this.body = new LinkedList<Coordinate>();
+        }
         public Snake(Coordinate initialPosition)
         {
             this.body = new LinkedList<Coordinate>();
             body.AddFirst(initialPosition);
+        }
+        public void Move(CoordDirection direction)
+        {
+            var newPosition = body.First.Value;
+            newPosition.Update(direction);
+            body.AddFirst(newPosition);
+            body.RemoveLast();
+        }
+        public void Eat(CoordDirection foodDirection)
+        {
+            var newPosition = body.First.Value;
+            newPosition.Update(foodDirection);
+            body.AddFirst(newPosition);
+        }
+
+        public List<Coordinate> getCoordinates()
+        {
+            return body.ToList<Coordinate>();
         }
     }
 }
