1f1cf556c30f0c4182399d8c80a798fd3fa86ef8
Donatas Kukta <donatas.kukta1@gmail.com>
2019-04-24 16:06:44 +0300

    Created custom linked list class that is suited to represent snake body object.

--- SnakeMultiplayer/wwwroot/js/CanvasRendering.js
+++ SnakeMultiplayer/wwwroot/js/CanvasRendering.js
@@ -1,5 +1,67 @@
 ﻿(function () {
     //DATA STRUCTURES
+class CustomLinkedListNode {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class CustomLinkedList {
+    constructor() {
+        this.count = 0;
+        this.head = null;
+        this.tail = null;
+        
+    }
+
+    addFirst(element) {
+        var newNode = new CustomLinkedListNode(element);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else if (this.count === 1) {
+            newNode.next = this.head;
+            this.head = newNode;
+            this.tail = this.head.next;
+            this.tail.previous = this.head;
+        } else {
+            newNode.next = this.head
+            this.head = newNode;
+            this.head.next.previous = this.head;
+        }
+        this.count += 1;
+    }
+
+    deleteLast() {
+        var deleted;
+        this.count -= 1;
+
+        if (this.head === null) {
+            console.warn("Trying to delete element from empty linked list!");
+            return null;
+        } else if (this.head.next == null) {
+            deleted = this.head.element;
+            this.head = null;
+            return deleted;
+        } else {
+            deleted = this.tail.element;
+            this.tail = this.tail.previous;
+            this.tail.next = null;
+            return deleted;
+        }
+    }
+
+    print() {
+        console.log("LinkedList elements:");
+        var node = this.head;
+        while (node !== null) {
+            console.log(node.element);
+            node = node.next;
+        }
+    }
+}
     class Cell {
         constructor(length = -1, innerColor, outlineColor, x = -1, y = -1) {
             this.size = length,
@@ -40,6 +102,14 @@
             DrawOutlineRectangle(cell.x, cell.y, cell.size, cell.outlineColor);
         }
     }
+
+    class Snake {
+        constructor(name, color) {
+            var x = new Array
+        }
+    }
+
+
     //---------------------------------
     //---------Entry point-------------
     //---------------------------------
@@ -59,6 +129,7 @@
     var CellsContainer;
     onResize();
 
+    var linkedList = new CustomLinkedList();
     //-----------------------
 
     function onResize() {
