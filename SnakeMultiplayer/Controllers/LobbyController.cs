046a7bfa2c201cce249bc279be39f1e568392cc3
Donatas Kukta <donatas.kukta1@gmail.com>
2019-05-03 21:23:16 +0300

    Required navigation structure: added Lobby controller and relevant views.

---
+++ SnakeMultiplayer/Controllers/LobbyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeMultiplayer.Controllers
+{
+    public class LobbyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GameArena(string Name)
+        {
+            ViewData["Name"] = Name;
+            return View();
+        }
+    }
+}
