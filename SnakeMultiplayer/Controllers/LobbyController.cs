1aa45b02fe05f716e316a61d91c5e929fd8555cc
Donatas Kukta <donatas.kukta1@gmail.com>
2019-05-11 15:39:59 +0300

    Implemented attribute routing relevant to new page structure.

--- SnakeMultiplayer/Controllers/LobbyController.cs
+++ SnakeMultiplayer/Controllers/LobbyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SnakeMultiplayer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,16 +7,45 @@ using System.Threading.Tasks;
 
 namespace SnakeMultiplayer.Controllers
 {
+    //[Route("Lobby")]
     public class LobbyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View("Views/Lobby/CreateLobby.cshtml");
         }
-        public IActionResult GameArena(string Name)
+        [HttpGet]
+        public IActionResult CreateLobby()
         {
-            ViewData["Name"] = Name;
             return View();
         }
+
+        [HttpPost]
+        public IActionResult CreateLobby(string id, string playerName)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult JoinLobby()
+        {
+            return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult JoinLobby(string id)
+        {
+            @ViewData["lobbyId"] = id;
+            return View();
+        }
+
+        //[HttpPost("/JoinLobby/{playerName}/{id}")]
+        [HttpPost]
+        public IActionResult JoinLobby(string id, string playerName)
+        {
+            return View();
+        }
+
     }
 }
