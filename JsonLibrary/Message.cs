12acb8f02b57cec7d44139613ac0e04b405bd532
Donatas Kukta <donatas.kukta1@gmail.com>
2019-05-29 13:10:45 +0300

    JSON class library: initial commit.

---
+++ JsonLibrary/Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace JsonLibrary
+{
+    public class Message
+    {
+        public string sender;
+        public string lobby;
+        public string type;
+        public string body;
+
+        public static Message Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<Message>(json);
+        }
+        public static string Serialize(Message m)
+        {
+            return JsonConvert.SerializeObject(m);
+        }
+    }
+}
---
+++ JsonLibrary/Strings.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace JsonLibrary
+{
+    public class Strings
+    {
+        public static string getString(byte[] array)
+        {
+            return Encoding.UTF8.GetString(array, 0, array.Length).Replace("\0", string.Empty);
+        }
+        public static byte[] getBytes(string text)
+        {
+            byte[] newBuffer = new byte[text.Length];
+            byte[] charsBuffer = Encoding.UTF8.GetBytes(text.ToCharArray());
+            Buffer.BlockCopy(charsBuffer, 0, newBuffer, 0, text.Length);
+            return newBuffer;
+        }
+        public static byte[] getBytes(string text, int bufferLength)
+        {
+            byte[] newBuffer = new byte[bufferLength];
+            byte[] charsBuffer = Encoding.UTF8.GetBytes(text.ToCharArray());
+            Buffer.BlockCopy(charsBuffer, 0, newBuffer, 0, text.Length);
+            return newBuffer;
+        }
+    }
+}
