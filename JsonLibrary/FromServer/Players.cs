1053fd47177c38fe4dc45e20bf86624b15e5e073
Donatas Kukta <donatas.kukta1@gmail.com>
2019-05-31 15:42:47 +0300

    Added classes that represent JSON "Players" type message content.

---
+++ JsonLibrary/FromServer/Players.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonLibrary
+{
+    public class Players
+    {
+        public List<Player> players;
+        public Players(List<Player> p)
+        {
+            this.players = p;
+        }
+
+        public static Players Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<Players>(json);
+        }
+        public static string Serialize(Players m)
+        {
+            return JsonConvert.SerializeObject(m);
+        }
+    }
+
+    public class Player
+    {
+        public string name;
+        public string color;
+        /// <summary>
+        /// 
+        /// </summary>
+        //List<string> body;
+    }
+
+}
