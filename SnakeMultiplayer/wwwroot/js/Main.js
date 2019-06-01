4a02e797909734b41d36257de2f75a0f06283960
Donatas Kukta <donatas.kukta1@gmail.com>
2019-06-01 16:23:32 +0300

    Added main disptcher that is responsible for event managemend between game controller and main; Added display manipulation of canvas element; Added method for updating player list; Added start button click event listener;

--- SnakeMultiplayer/wwwroot/js/Main.js
+++ SnakeMultiplayer/wwwroot/js/Main.js
@@ -11,18 +11,19 @@
         // Redirect to error page.
     }
 
+    if (document.getElementById("startButton") !== null) {
+    document.getElementById("startButton").onclick = onStartGameButtonClick;
+    }
+
+
     window.addEventListener('resize', reSet, false);
-    var gameController = new GameController(PlayerName, LobbyId);
-    
+    var MainDispatcher = new Dispatcher();
+    MainDispatcher.on("onPlayerListReceived", updatePlayers.bind(this));
+
+    var gameController = new GameController(PlayerName, LobbyId, MainDispatcher);
     gameController.setEnvironment();
     gameController.setCellContainer(new CellGridContainer(cellCount, baseCell, CanvasContext, TLborder, BRborder)); gameController.doStubActions();
     
-    //Absurdsss
-    /*
-    gameController.socket.onopen = function (e) {
-        gameController.send("inicijuota...");
-        gameController.send("inicijuota23..");
-    }*/
     
     document.onkeydown = function (e) {
         switch (e.key) {
@@ -41,14 +42,72 @@
         }
     };
 
-    function metodasKazkoks() {
-        console.log("iskviestas metodas");
-    }
-
     function reSet() {
         onResize();
         gameController.setCellContainer(new CellGridContainer(cellCount, baseCell, CanvasContext, TLborder, BRborder));
         gameController.drawSnakes();       
+    }
+
+    function onStartGameButtonClick(e) {
+        console.warn("Iskviestas pradėjimo mygtukas");
+        
+    }
+
+    function onCountDownEvent(e) {
+
+    }
+
+    function onGameStartRececeived(e) {
+
+        showCanvas();
+    }
+
+    function onGameEndReceived(e) {
+        //re-set canvas state
+        hideCanvas();
+    }
+
+    function showCanvas() {
+        var element = document.getElementById('Canvas');
+        console.log("canvas element:", element);
+        element.style.visibility = 'visible';
+    }
+
+    function hideCanvas() {
+        var element = document.getElementById('Canvas');
+        console.log("canvas element:", element);
+        //element.style.display = 'none'; //or
+        element.style.visibility = 'hidden';
+    }
+
+    function updatePlayers(players) {
+        console.log("HTML Received update players: ", players);
+
+        if (!Array.isArray(players)) {
+            console.error("Not error received at update table: ", players);
+        }
+
+        var playerCardList = document.getElementById("playerCards");
+        while (playerCardList.hasChildNodes()) {
+            playerCardList.removeChild(playerCardList.firstChild);
+        }
+        /* Card html example
+     <div class="playerCard card" style="background-color: greenyellow;">
+        <h5 class="card-header">Donatas</h5>
+    </div>
+         */
+
+        players.forEach(function (player) {
+            var playerCard = document.createElement("div");
+            playerCard.className = "playerCard card";
+            playerCard.style = "background-color: " + player.color + ";";
+            //playerCard.innerHTML = "<h5 class=\"card-header\"> " + player.name + "</h5> <div class=\"card-body\"><p class=\"card-text\">" + player.type + "</p></div>";
+            var hostString = (player.type === "Host") ? " [host]" : "";
+            playerCard.innerHTML = "<h5 class=\"card-header\"> " + player.name + hostString + "</h5>";
+            console.log("Kortele:", playerCard);
+            playerCardList.appendChild(playerCard);
+            console.log("Naujas sarasas: ", playerCardList);
+        });
     }
 
     function onSnakeMovement() {
