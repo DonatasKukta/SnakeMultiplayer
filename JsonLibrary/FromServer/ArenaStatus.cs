b5db837e573d296510f64a2ce49f92c7de0f9dec
Donatas Kukta <donatas.kukta1@gmail.com>
2019-06-02 12:09:02 +0300

    Created class that represents JSON update object sent from server

---
+++ JsonLibrary/FromServer/ArenaStatus.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonLibrary
+{
+    [JsonObject]
+    public class ArenaStatus
+    {
+        public List<Snake> snakes;
+        public XY food;
+
+        public ArenaStatus(XY food)
+        {
+            this.food = food;
+            snakes = new List<Snake>();
+        }
+        public void  AddSnake(Snake s)
+        {
+            snakes.Add(s);
+        }
+
+        public static Players Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject<Players>(json);
+        }
+        public static string Serialize(Players m)
+        {
+            return JsonConvert.SerializeObject(m);
+        }
+    }
+    [JsonObject]
+    public class Snake
+    {
+        public List<XY> body;
+        public string player;
+        public Snake(string player)
+        {
+            this.player = player;
+            body = new List<XY>();
+        }
+
+        public void AddCoord(XY c)
+        {
+            body.Add(c);
+        }
+    }
+    [JsonObject]
+    public class XY
+    {
+        public int x;
+        public int y;
+        public XY(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
