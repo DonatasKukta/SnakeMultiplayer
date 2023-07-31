64b95a72b3a5088ce436bfd9191ada163899c08c
Donatas Kukta <donatas.kukta1@gmail.com>
2023-07-31 12:32:19 +0300

    Add config for vscode development.

--- SnakeMultiplayer/Program.cs
+++ SnakeMultiplayer/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using SnakeMultiplayer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
